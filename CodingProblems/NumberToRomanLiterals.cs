89632c2e45979505c589bc129972787998b72742
Yossi <yossi.sternlicht@wealth-dynamix.com>
2022-10-07 16:12:17 +0100

    Add GetCommonSubstring and bare Roman method

---
+++ CodingProblems/NumberToRomanLiterals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+    public class NumberToRomanLiterals
+    {
+        public string ConvertNumberToRomanLiteral(int number)
+        {
+            return "";
+        }
+    }
+}
